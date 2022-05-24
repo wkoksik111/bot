fbe3c47ebf6007d469575454517f9f1262603183
wkoksik111 <102420085+wkoksik111@users.noreply.github.com>
2022-05-24 21:15:16 +0200

    Add files via upload

---
+++ levele2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WOmodLite
+{
+    public partial class levele2 : Form
+    {
+        public levele2()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ tworzenie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WOmodLite
+{
+    public partial class tworzenie : Form
+    {
+        public tworzenie()
+        {
+            InitializeComponent();
+        }
+    }
+}
